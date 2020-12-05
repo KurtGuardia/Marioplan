4aafed7fd29d6e66d071037e8d042d218dd1f2cd
Kurt Guardia <kurtguardia@gmail.com>
2020-12-05 09:01:19 +0100

    All layout components created

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
 
 const Navbar = () => {
   return (
-    <div className="nav-wrapper grey darken-3">
+    <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MArioPlan
+          MarioPlan
         </Link>
+        <SignedInLinks />
+        <SignedOutLinks />
       </div>
-    </div>
+    </nav>
   );
 };
 
