4ed94a5190ecb6865adacc8420cf58f5023d9f09
Kurt Guardia <kurtguardia@gmail.com>
2020-12-15 10:21:29 +0100

    Added formated dates with Moment.js

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,34 +1,35 @@
-import React from "react";
-import { connect } from "react-redux";
-import { firestoreConnect } from "react-redux-firebase";
-import { compose } from "redux";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
 
-  if (!auth.uid) return <Redirect to="/signin" />;
+  if (!auth.uid) return <Redirect to='/signin' />;
 
   if (project) {
     return (
-      <div className="container section project-details">
-        <div className="card z-depth-0">
-          <div className="card-content">
-            <span className="card-title">{project.title}</span>
+      <div className='container section project-details'>
+        <div className='card z-depth-0'>
+          <div className='card-content'>
+            <span className='card-title'>{project.title}</span>
             <p>{project.content}</p>
           </div>
-          <div className="card-action frey lighten-4 grey-text">
+          <div className='card-action frey lighten-4 grey-text'>
             <div>
               Posted by {project.authorFirstName} {project.authorLastName}
             </div>
-            <div>9th December, 7am</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
     );
   } else {
     return (
-      <div className="container center">
+      <div className='container center'>
         <p>Loading project...</p>
       </div>
     );
@@ -50,7 +51,7 @@ export default compose(
   connect(mapStateToProps),
   firestoreConnect([
     {
-      collection: "projects",
+      collection: 'projects',
     },
   ])
 )(ProjectDetails);
