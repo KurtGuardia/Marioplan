dd722f2794ef8e1981cbd684848264f5a7ff5ebd
Kurt Guardia <kurtguardia@gmail.com>
2020-12-17 13:51:23 +0100

    setting hosting files

---
+++ dist/precache-manifest.fabd14dccdf09783f9cc08bc71af8554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587243b7018f5e5dba24da2462508ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7602610eb2c9ae8210a6",
+    "url": "/static/css/main.b05484a8.chunk.css"
+  },
+  {
+    "revision": "a0827b5c9b597c01560b",
+    "url": "/static/js/2.cb5b888d.chunk.js"
+  },
+  {
+    "revision": "dc3986ca2d930c2ca7454532b050e839",
+    "url": "/static/js/2.cb5b888d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7602610eb2c9ae8210a6",
+    "url": "/static/js/main.e0cd43c1.chunk.js"
+  },
+  {
+    "revision": "acdf868312f78a97cdd0",
+    "url": "/static/js/runtime-main.794c9aa6.js"
+  }
+]);
