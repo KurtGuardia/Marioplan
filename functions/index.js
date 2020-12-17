af5d697ad50f384e10e16f9b067873f3ad7948d9
Kurt Guardia <kurtguardia@gmail.com>
2020-12-17 13:21:26 +0100

    created function to notify when new user is added

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const createNotificaion = ((notification) => {
     .firestore()
     .collection('notifications')
     .add(notification)
-    .then((doc) => console.log('notification added'));
+    .then((doc) => console.log('notification added: ', doc));
 });
 
 exports.projectCreated = functions.firestore
@@ -22,3 +22,15 @@ exports.projectCreated = functions.firestore
     };
     return createNotificaion(notification);
   });
+
+  exports.userJoined = functions.auth.user().onCreate(user=>{
+      return admin.firestore().collection('users').doc(user.uid).get().then(doc => {
+          const newUser = doc.data()
+          const notification = {
+            content: 'Joined the party',
+            user: `${newUser.firstName} ${newUser.lastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+          };
+           return createNotificaion(notification);
+      })
+  })
